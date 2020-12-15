1d15ae4461d406b17896c3c1e206ed6ad6a6e529
RyutaUematsu <r.resolute.pro@gmail.com>
2020-12-16 03:27:10 +0900

    This is twitter interrupter. App No 001

---
+++ twitter-interrupter-001/main.py
@@ -0,0 +1,19 @@
+import pygetwindow as gw
+import time
+
+
+INTERRUPT_TIMEOUT = 3     # The Twitter window will close after this time(min) 
+
+cnt = 0
+while True:
+    time.sleep(1)
+    if 'Twitter' in gw.getAllTitles():
+        cnt += 1
+    if cnt == INTERRUPT_TIMEOUT*60:
+        try:
+            cnt = 0
+            target  = gw.getWindowsWithTitle('Twitter')[0]
+            target.close()
+        except Exception as e:
+            print(e)
+        
